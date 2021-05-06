e67d54a34e544bafd223bfe070beb4d19e93569b
jbudny <jbudny8@gmail.com>
2021-05-07 00:28:44 +0200

    Move gltf files under src directory so they can be reached by tests

--- src/components/3DModels/Ground/Ground.tsx
+++ src/components/3DModels/Ground/Ground.tsx
@@ -2,13 +2,12 @@ import { useGLTF } from '@react-three/drei'
 import React, { useRef } from 'react'
 import * as THREE from 'three'
 
+import GroundGLTF from '../../../blenderFiles/gltf/Ground/Ground.gltf'
 import { GroundGLTFResult, GroundProps } from './Ground.types'
-
-const path = './GLTFModels/Ground.gltf'
 
 const Ground: React.FC<GroundProps> = ({ color, ...props }: GroundProps) => {
 	const group = useRef<THREE.Group>()
-	const { nodes, materials } = useGLTF(path) as GroundGLTFResult
+	const { nodes, materials } = useGLTF(GroundGLTF) as GroundGLTFResult
 	if (color) materials.GroundMaterial.color.set(color)
 
 	return (
@@ -29,4 +28,4 @@ const Ground: React.FC<GroundProps> = ({ color, ...props }: GroundProps) => {
 
 export default Ground
 
-useGLTF.preload(path)
+useGLTF.preload(GroundGLTF)
